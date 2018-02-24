fe55b25af91d0fa811acc2eef8dacbc1dd6e5ec7
M.Mokhles <moha199612@gmail.com>
2018-02-24 21:03:25 +0200

    Added admin views
    admin views for complaints and LC_Requests

--- illuminate/complaints/admin.py
+++ illuminate/complaints/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Complaint
+from .forms import ComplaintForm
+
+class ComplaintAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'whatsapp_number', 'email', 'country', 'program', 'host_lc', 'complaint','complaint_tag',"timestamp","updated"]
+    form = ComplaintForm
+    class Meta:
+        model = Complaint
+
+
+admin.site.register(Complaint)
+
--- illuminate/lc_requests/admin.py
+++ illuminate/lc_requests/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Request
+from .forms import RequestForm
+
+class RequestAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'lc', 'position', 'role',
+                  'ep_name', 'ep_entity', 'ep_email','ep_lc','ep_id','opp_id',
+                  'requested_break','program','request_Reason',
+                  "timestamp","updated"]
+    form = RequestForm
+    class Meta:
+        model = Request
+
+
+admin.site.register(Request)
